179efe7f77110d349894017c03f2cb2be1c716b9
Qilal Hafaf <qilalhafaf@gmail.com>
2025-08-13 17:41:36 +0700

    belajar Jawa Script confirm-promt-while-for

--- latihan-2.js
+++ latihan-2.js
@@ -1,1 +1,18 @@
-alert("ini pop up")
+
+// menggunakan for
+// for (var test=false; test === false; !test) {
+//     var soal1 = prompt("2+2= ?");
+//     var test = confirm("Apakah kamu yakin dengan jawabanmu?");
+// }
+
+// menggunakan while 
+// while (!test) {
+//     var soal1 = prompt("2+2= ?");
+//     var test = confirm("Apakah kamu yakin dengan jawabanmu?");
+// }
+
+// if (soal1 === '4') {
+//     alert("Jawaban benar");
+// } else {
+//     alert("Jawaban salah");
+// }
